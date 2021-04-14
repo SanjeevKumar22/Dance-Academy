56dcebf8a7c1681bc45cfabf42e263668befea36
SanjeevKumar22 <74849833+SanjeevKumar22@users.noreply.github.com>
2021-04-14 17:43:43 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path =require("path");
+const app=express();
+const port=8000;
+//Express specific stuff
+app.use('/static',express.static('static'))//for serving static files
+app.use(express.urlencoded())
+//pug specific stuff
+app.set('view engine','pug')//set the template engine as pug
+app.set('views',path.join(__dirname,'views'))//set the views directory
+
+// ENDPOINTS
+app.get('/', (req, res)=>{
+    const params = {}
+    res.status(200).render('home.pug', params);
+})
+app.get('/contact', (req, res)=>{
+    const params = {}
+    res.status(200).render('contact.pug', params);
+})
+
+// START THE SERVER
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
+});
